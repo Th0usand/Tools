5b430e12e554f5411513e6082589d1b56971e47f
Thousand <38495126+Th0usand@users.noreply.github.com>
2021-03-10 11:23:20 +0800

    Update fofa.py

--- python3/fofa.py
+++ python3/fofa.py
@@ -9,7 +9,6 @@ import base64
 def fofa(str_url):
         # -----------------------------------------------------------
     url="https://fofa.so/api/v1/search/all?email=[xxxxx]&key=[xxxxx]&qbase64="+str_url
-    #header={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) Applewebkit/537.36(KHTML,like gecko) Chrome/63.0.3239.132 Safari/537.36'}
     r = requests.get(url,'size=9999',verify=False)
     print(r.url)
     js = r.json()
@@ -36,9 +35,7 @@ if __name__ == '__main__':
     # -----------------------------------------------------------
     arg = 'app="xxxxx"'
     print(arg)
-    # str_url = base64.b64encode(url)
     str_url = base64.b64encode(arg.encode('utf-8'))
     str_url = str(str_url,'utf-8')
     print(str_url)
     fofa(str_url)
-
