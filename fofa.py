94976da6f5ac8958b4ce4f94a0bf3d7c0e28a07e
Thousand <38495126+Th0usand@users.noreply.github.com>
2021-03-10 11:08:57 +0800

    Create fofa.py

---
+++ fofa.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import requests
+import optparse
+import json
+import re
+import base64
+
+
+def fofa(str_url):
+        # -----------------------------------------------------------
+    url="https://fofa.so/api/v1/search/all?email=[xxxxx]&key=[xxxxx]&qbase64="+str_url
+    #header={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) Applewebkit/537.36(KHTML,like gecko) Chrome/63.0.3239.132 Safari/537.36'}
+    r = requests.get(url,'size=9999',verify=False)
+    print(r.url)
+    js = r.json()
+
+
+    for i in js['results']:
+        isis =  i[0]
+        if "https" not in isis:
+            tmd = "http://" + i[0]
+        else:
+            tmd = ''
+
+        nam = tmd
+        print(nam)
+
+
+        #---------------------------------------------------------------
+        f1 = open('xxxxx.txt', 'a+')
+        f1.write(nam + '\n')
+        f1.close()
+
+
+if __name__ == '__main__':
+
+    # -----------------------------------------------------------
+    arg = 'app="xxxxx"'
+    print(arg)
+    # str_url = base64.b64encode(url)
+    str_url = base64.b64encode(arg.encode('utf-8'))
+    str_url = str(str_url,'utf-8')
+    print(str_url)
+    fofa(str_url)
+
